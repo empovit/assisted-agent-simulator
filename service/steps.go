f8a2df0c43461168b6ef724e4e0ff52a9e38455b
Vitaliy Emporopulo <vemporop@redhat.com>
2020-10-21 12:48:00 +0300

    Move steps to a separate file

---
+++ service/steps.go
@@ -0,0 +1,15 @@
+package service
+
+import "github.com/empovit/assisted-agent-simulator/service/models"
+
+// Steps are instructions to be returned to the command runner
+var Steps = []models.Step{
+	models.Step{
+		Command: "bash",
+		Args:    []string{"-c", "podman run --rm alpine:latest sleep 120"},
+	},
+	models.Step{
+		Command: "bash",
+		Args:    []string{"-c", "podman stop `podman ps --format \"{{.ID}} {{.Names}}\" | grep -v agent-simulator | awk \"{print $1}\"`; echo \"Finished\""},
+	},
+}
